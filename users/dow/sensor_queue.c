f5a26fbadf684779e1bcb353b4cd2cd2cc2fd97f
Dow Perkins <walter21@vt.edu>
2018-01-25 12:24:52 -0500

    edited sensor queue for better error visibility and decoupling from debug

--- users/dow/sensor_queue.c
+++ users/dow/sensor_queue.c
@@ -8,27 +8,30 @@
 
 #include "sensor_queue.h"
 
-QueueHandle_t queueInit(int queueSize, int itemSize){
-    QueueHandle_t handle = xQueueCreate(queueSize, itemSize);
+int queueInit(QueueHandle_t * handle, int queueSize, int itemSize){
+    *handle = xQueueCreate(queueSize, itemSize);
     
-    if(handle == NULL){
-        dbgStopAll(1); //change hardcoded value
-    }
-}
-
-void queueSend(QueueHandle_t queue, unsigned int itemSent){
-    if(xQueueSend(queue, itemSent, 25) != pdTRUE){ //change hardcoded wait time
-        dbgStopAll(1); //change hardcoded value
-    }
-}
-
-unsigned int queueReceive(QueueHandle_t queue){
-    unsigned int * valueReceived;
-    if(xQueueReceive(queue, valueReceived, 25) != pdTRUE){ //change hardcode
-        dbgStopAll(1); //change hardcode
+    if(*handle == NULL){
+        return -1;
     }
     
-    return *valueReceived;
+    return 0;
+}
+
+int queueSend(QueueHandle_t queue, unsigned int itemSent){
+    if(xQueueSend(queue, itemSent, QUEUE_WAIT) != pdTRUE){
+        return -1;
+    }
+    
+    return 0;
+}
+
+int queueReceive(QueueHandle_t queue, unsigned int * itemRec){
+    if(xQueueReceive(queue, itemRec, QUEUE_WAIT) != pdTRUE){
+        return -1;
+    }
+    
+    return 0;
 }
 
 void queueDestroy(QueueHandle_t queue){
--- users/dow/sensor_queue.h
+++ users/dow/sensor_queue.h
@@ -7,45 +7,47 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "FreeRTOS.h"
 #include "queue.h"
-#include "debug.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // DEFINEs
 ////////////////////////////////////////////////////////////////////////////////
+
+#define QUEUE_WAIT 25
 
 ////////////////////////////////////////////////////////////////////////////////
 // FUNCTIONs
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-// Function:    queueInit(int queueSize, int itemSize)
+// Function:    queueInit(QueueHandle_t * handle, int queueSize, int itemSize)
 // Inputs:      expects two ints, one is the queue size in words, one is the 
-// item size in bytes
-// Outputs:     A handle to the created queue
+// item size in bytes, and handle pointer
+// Outputs:     Integer containing error value. 0 on success.
 // Description: Creates a FreeRTOS queue using the API functions provided.
 ////////////////////////////////////////////////////////////////////////////////
 
-QueueHandle_t queueInit(int queueSize, int itemSize);
+int queueInit(QueueHandle_t * handle, int queueSize, int itemSize);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Function:    queueSend(QueueHandle_t queue, unsigned int itemSent)
 // Inputs:      Expects handle to queue to send to, and the item to send
-// Outputs:     N/A
+// Outputs:     Integer containing error value. 0 on success.
 // Description: Sends a value to the FreeRTOS queue using the API functions
 // provided.
 ////////////////////////////////////////////////////////////////////////////////
 
-void queueSend(QueueHandle_t queue, unsigned int itemSent);
+int queueSend(QueueHandle_t queue, unsigned int itemSent);
 
 ////////////////////////////////////////////////////////////////////////////////
-// Function:    queueReceive(QueueHandle_t queue)
-// Inputs:      Expects the handle of the queue to receive from
-// Outputs:     Received unsigned int value
+// Function:    queueReceive(QueueHandle_t queue, unsigned int * itemRec)
+// Inputs:      Expects the handle of the queue to receive from and a pointer to
+// the value received.
+// Outputs:     Integer containing error value. 0 on success.
 // Description: Receives a value from the FreeRTOS queue using the API functions
 // provided.
 ////////////////////////////////////////////////////////////////////////////////
 
-unsigned int queueReceive(QueueHandle_t queue);
+int queueReceive(QueueHandle_t queue, unsigned int * itemRec);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Function:    queueDestroy(QueueHandle_t queue);
